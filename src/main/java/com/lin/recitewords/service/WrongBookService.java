c56a9f228d4b9ec74d2d343acc425169323a82ad
猴哥 <l243920161@163.com>
2019-03-14 16:36:25 +0800

    增加了查询单词接口

--- src/main/java/com/lin/recitewords/service/WrongBookService.java
+++ src/main/java/com/lin/recitewords/service/WrongBookService.java
@@ -2,6 +2,7 @@ package com.lin.recitewords.service;
 
 import com.lin.recitewords.common.service.AbstractService;
 import com.lin.recitewords.mapper.WrongBookMapper;
+import com.lin.recitewords.model.WordBook;
 import com.lin.recitewords.model.WrongBook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ import java.util.List;
 public class WrongBookService extends AbstractService<WrongBook> {
 	@Autowired(required = false)
 	private WrongBookMapper wrongBookMapper;
+
+	// 根据用户id查找单词本
 	public List<WrongBook> findByUserId(Integer userId) {
 		return wrongBookMapper.findByUserId(userId);
 	}
